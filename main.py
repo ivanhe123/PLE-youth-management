36b80314ae006d9b6ca6d32eb2230095909aa362
IGoByALotOfNames <108038846+ivanhe123@users.noreply.github.com>
2025-03-23 07:32:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,19 @@ selected_language = st.sidebar.selectbox(
 )
 
 
+# Get the appropriate pages based on the selected language.
+pages = [
+            st.Page("pages/enrollment_forum.py"),
+            st.Page("pages/admin_panel.py"),
+
+            st.Page("pages/teacher_panel.py"),
+            st.Page("pages/enrollment_forum_mand.py"),
+            st.Page("pages/admin_panel_mand.py"),
+
+            st.Page("pages/teacher_panel_mand.py"),
+        ]
+
+nav = st.navigation(pages=pages)
 get_pages(selected_language)
+
+nav.run()
