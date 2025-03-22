bfa47e298ee21947db5c18726a04ea3243b07523
IGoByALotOfNames <108038846+ivanhe123@users.noreply.github.com>
2025-03-23 07:48:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,6 @@ import streamlit as st
 from streamlit_javascript import st_javascript
 
 
-def go_to_url(url) -> None:
-
-    st.html ( f"""
-    <script>
-        window.open("{url}", "_blank");
-    </script>
-    """)
-
-
-# Add a language dropdown to the sidebar.
-# Using an on_change callback with st.experimental_rerun ensures the app refreshes when selection changes.
-lang_options = ["English (英语) ", "中文 (Chinese)"]
-selected_language = st.sidebar.selectbox(
-    "Select Language/选择一个语言",
-    lang_options,
-)
-
 
 # Get the appropriate pages based on the selected language.
 pages = [
