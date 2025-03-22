41c99e38cb7c21574f593a157fd20f57441513c9
IGoByALotOfNames <108038846+ivanhe123@users.noreply.github.com>
2025-03-23 06:09:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from streamlit_javascript import st_javascript
 def get_pages(language: str):
     """Return the list of pages based on the selected language."""
 
-    url = st_javascript("await fetch('').then(r => window.parent.location.href)")
+    url = st_javascript("window.parent.location.href")
     st.write(url)
     if language == "中文 (Chinese)":
         return [
