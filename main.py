b2fb17d2d8001b3dc623619d0ed4df03395fbdf7
IGoByALotOfNames <108038846+ivanhe123@users.noreply.github.com>
2025-03-23 08:18:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ if "selected_language" not in st.session_state:
 lang_options = ["English (英语)", "中文 (Chinese)"]
 selected_language = st.sidebar.selectbox("Select Language/选择语言", lang_options)
 st.session_state.selected_language = selected_language
+pages= [ st.Page("enrollment_forum.py"), st.Page("admin_panel.py"), st.Page("teacher_panel.py"), ]
+nav = st.navigation(pages=pages, position="hidden")
+nav.run()
